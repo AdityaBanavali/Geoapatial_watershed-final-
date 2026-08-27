f1a756b4a883647492b0d7a2172e413b2c647db7
Aditya Banavali <adityabanavali@Adityas-MacBook-Air.local>
2026-08-27 14:42:02 +0530

    fix: finalize dockerfile command and python entrypoint

--- main.py
+++ main.py
@@ -407,5 +407,7 @@ def watershed_analysis(payload: dict[str, Any] = Body(...)) -> dict[str, Any]:
 
 if __name__ == "__main__":
     import uvicorn
-    port = int(os.getenv("PORT", 8080))
+    import os
+
+    port = int(os.environ.get("PORT", 8080))
     uvicorn.run("main:app", host="0.0.0.0", port=port)
